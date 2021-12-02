ffca69412c8dde9c925ee8994cf0f35865594c07
Ricardo Proença <ricardo.proenca@uphold.com>
2021-12-02 18:25:53 +0000

    Add GA to automatically update code from libphonenumber

--- bin/update.sh
+++ bin/update.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 
+# Make sure we are in the master branch.
+git checkout master
+
 # Get current working dir.
 PWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# Test version argument.
-if [ -z "$1" ]; then
-  echo "A release version must be supplied (e.g. 7.0.1)."
-
-  exit 1
-fi
-
 # Remove previous src dir.
-find src -depth 1 ! -iregex 'src/index.js' | xargs rm -rf
+find src -depth -mindepth 1 ! -iregex 'src/index.js' | xargs rm -rf
 
-# Prepare a new branch for this update.
-git checkout -b support/update-libphonenumber-${1//\./\-}
+REPOSITORY_DATA=$(curl --silent "https://api.github.com/repos/google/libphonenumber/releases/latest")
+LATEST_TAG=$(echo $REPOSITORY_DATA | grep -Po '"tag_name": "v\K.*?(?=")')
 
 # Download the requested tagged release.
-echo "Downloading release $1..."
+echo "Downloading release $LATEST_TAG..."
 
-curl -L -s https://github.com/googlei18n/libphonenumber/archive/v$1.tar.gz -o v$1.tar.gz
+curl -L -s https://github.com/googlei18n/libphonenumber/archive/v$LATEST_TAG.tar.gz -o v$LATEST_TAG.tar.gz
 
-tar -xf v$1.tar.gz
+tar -xf v$LATEST_TAG.tar.gz
 
-cp libphonenumber-$1/javascript/i18n/phonenumbers/* src/
+cp libphonenumber-$LATEST_TAG/javascript/i18n/phonenumbers/* src/
 
-rm v$1.tar.gz
+rm v$LATEST_TAG.tar.gz
 
-rm -rf libphonenumber-$1
-
-echo "Done!"
+rm -rf libphonenumber-$LATEST_TAG
 
 # Add the modified files to git.
 git add $PWD/../src/
 
-# Commit with the standard message.
-git commit -m "Update libphonenumber@$1"
+if [ $(git diff --cached --name-only | wc --lines) -gt 0 ]; then
+  # Prepare a new branch for this update.
+  git checkout -b support/update-libphonenumber-${LATEST_TAG//\./\-}
+
+  git config --global user.email "github-actions[bot]@users.noreply.github.com"
+  git config --global user.name "github-actions[bot]"
+
+  # Commit with the standard message.
+  git commit -m "Update libphonenumber@$LATEST_TAG"
+
+  # Push the new branch.
+  git push -u origin support/update-libphonenumber-${LATEST_TAG//\./\-}
+fi;
+
+echo "Done!"
